5e0209bd61d5db0002c0643b15eace8d4f3191e3
Aiden Hu <125307388+TheCoder122@users.noreply.github.com>
2025-09-25 10:44:25 -0700

    shopping

---
+++ app.js
@@ -0,0 +1,51 @@
+/*
+let x = 5;
+x = 6;
+console.log(x);
+
+const x = 5;
+x = 6;
+//console.log(x);
+
+*/
+//strings represents names, characters letters etc
+/*
+const x="Ian";
+
+//integers/numbers
+const y=13245
+
+//boolean true or false
+const citizen = true;
+
+//null is the absence of value
+let name = null;
+
+//undefined
+let z; 
+/*
+
+//functions are input output machines
+//functions accept inputs or paremeters
+//declaring and defining a function
+function add(x,y){
+    consle.log(x+y);
+}
+    return x+y;
+}
+    //run the function
+    const bill = add (9,8);
+    console.log(bill);
+
+const x=7
+function returnThing(){
+//function scope or local scope, only accessible inside this function
+    const x=8
+    console.log(x);
+}
+    returnThing();}
+/*
+const name="Nelson";
+console.log('OMG everyone hates ${name}!');
+
+console.log(5% 2);
